5b35734c8be1691e8888299b9650e029a9ce11cf
Geoff Kelly <geoffkellync@gmail.com>
2023-12-29 21:05:53 -0500

    initiated google analytics on production site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './views/home/Home'
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-6YS6GC8BQ5');
+    ReactGA.pageview(location.pathname + location.search);
+  } , [location]);
+
   return (
     <div className="app-body">
       <Routes>
